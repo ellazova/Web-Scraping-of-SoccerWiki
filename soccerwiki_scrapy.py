fee17cc5189ac4fe98ea7a8db34a97fc3126efa0
Leyla Ellazova <100853330+ellazova@users.noreply.github.com>
2022-05-14 20:43:24 +0200

    Add files via upload

---
+++ soccerwiki_scrapy.py
@@ -0,0 +1,104 @@
+# import  our module files
+import scrapy
+import pandas as pd
+
+# main url and getting urls
+base_url = "https://en.soccerwiki.org/search/player?minrating=90&maxrating=99"
+next_urls = [base_url]
+
+# create pages
+for page in range(15,121,15):
+    next_payload = "&offset=" + str(page)
+
+    if page == 15:
+        next_url = base_url + next_payload
+        next_urls.append(next_url)
+    elif page == 30:
+        next_url = next_urls[-1] + next_payload
+        next_urls.append(next_url)
+    elif page == 45:
+        next_url = next_urls[-1] + next_payload
+        next_urls.append(next_url)
+    elif page == 60:
+        next_url = next_urls[-1] + next_payload
+        next_urls.append(next_url)
+    elif page == 75:
+        next_url = next_urls[-1] + next_payload
+        next_urls.append(next_url)
+    elif page == 90:
+        next_url = next_urls[-1] + next_payload
+        next_urls.append(next_url)
+    elif page == 105:
+        next_url = next_urls[-1] + next_payload
+        next_urls.append(next_url)
+    else:
+        pass
+
+
+# inherit from scrapy
+class Soccer(scrapy.Spider):
+    
+    # name of our spider
+    name = 'soccer_spider'
+    
+    # Our Start URLS 
+    start_urls = next_urls 
+    
+    # Later write to file
+    output = "scrapy_player_stats.csv"
+    
+    # player STATS
+    stats_list = list()
+
+    # Parse our HTML with scrapy's perse
+    def parse(self, response):
+        # Get the response from the get_request(start_urls)
+        all_stats = response.css("tbody > tr")
+        
+        # Basically looping through table rows under the body & finding where our data resides
+
+        for stat in all_stats:
+            NAME = ".//td[@class='text-left']/a/text()"
+            CLUB = './/td[@class="text-left"]/a/text()'
+            POSITION = './/td[@class="text-left text-dark"]/span/text()'
+            HEIGHT = './/td[@class="text-center text-dark"]/text()'
+            FOOT = './/td[@class="text-center text-dark"]/text()'
+            AGE = './/td[@class="text-center text-dark"]/text()'
+            RATING = './/td[@class="text-center text-dark"]/text()'
+            
+            # empty stats dict
+            STATS = dict()
+
+            # update our stats dictionary with join method that converts list to string
+            STATS["NAME"] = ' '.join(stat.xpath(NAME)[0].getall())
+            STATS["CLUB"] = ' '.join(stat.xpath(CLUB)[1].getall())
+            STATS["POSITION"] = stat.xpath(POSITION).get()
+            STATS["HEIGHT"] = ' '.join(stat.xpath(HEIGHT)[0].getall())
+            STATS["FOOT"] = ' '.join(stat.xpath(FOOT)[1].getall())
+            STATS["AGE"] = ' '.join(stat.xpath(AGE)[2].getall())
+            STATS["RATING"] = ' '.join(stat.xpath(RATING)[3].getall())
+            
+            # test
+            # print(STATS["NAME"], STATS["CLUB"],STATS["POSITION"],STATS["HEIGHT"], STATS["FOOT"], STATS["AGE"], STATS["RATING"])
+
+            # update our stats fil
+            self.stats_list.append((STATS["NAME"], STATS["CLUB"],STATS["POSITION"],STATS["HEIGHT"], STATS["FOOT"], STATS["AGE"],STATS["RATING"]))
+            # print("--on it--")
+            
+            print("getting data ....")
+        
+        # get only top 100
+        top_100_players = self.stats_list[0:101]
+        # print(top_100_players)
+        
+        #Include Headers in our CSV
+        HEADERS = ["NAME", "CLUB", "POSITION", "HEIGHT", "FOOT", "AGE" , "RATING"]
+        
+        # CREATE A stats pandas DATAFRAME
+        stats_dataframe = pd.DataFrame(top_100_players, columns=HEADERS)
+        
+        # Open our output file & write to it
+        stats_dataframe.to_csv(self.output)
+        
+        print(f"Updated {self.output} File")
+
---
+++ soccerwiki_soup.py
@@ -0,0 +1,100 @@
+# modules to use
+from bs4 import BeautifulSoup 
+import requests
+import pandas as pd
+
+# create an http header
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+ Chrome/86.0.4240.75 Safari/537.36'}
+
+base_url = "https://en.soccerwiki.org/search/player?minrating=90&maxrating=99"
+
+# put stats here
+player_stats = []
+
+# write to file later
+output_file = "soup_player_stats.csv"
+
+# All URLS we need
+next_urls = [base_url]
+URLS = next_urls
+
+# build urls on from base_url
+def build_urls():
+    for page in range(15,121,15):
+        next_payload = "&offset=" + str(page)
+        
+        if page == 15:
+            next_url = base_url + next_payload
+            next_urls.append(next_url)
+        elif page == 30:
+            next_url = next_urls[-1] + next_payload
+            next_urls.append(next_url)
+        elif page == 45:
+            next_url = next_urls[-1] + next_payload
+            next_urls.append(next_url)
+        elif page == 60:
+            next_url = next_urls[-1] + next_payload
+            next_urls.append(next_url)
+        elif page == 75:
+            next_url = next_urls[-1] + next_payload
+            next_urls.append(next_url)
+        elif page == 90:
+            next_url = next_urls[-1] + next_payload
+            next_urls.append(next_url)
+        elif page == 105:
+            next_url = next_urls[-1] + next_payload
+            next_urls.append(next_url)
+        else:
+            pass
+    print("urls built for scraping")
+# print(next_urls)
+
+# get stats from urls 
+def get_stats():
+    for url in URLS:
+        response = requests.get(url, headers=headers)
+        # Let's create a  of our soup to parse the HTML for operations using  "lxml"    
+        soup = BeautifulSoup(response.content.decode(), "html.parser")
+
+        # get the body of stats
+        stats_body = soup.find("tbody").findAll('tr')
+        
+        # test
+        # print(stats_body)
+        
+        # Loop through and get the data from the body;
+        for stat in stats_body:        
+            NAME = stat.find_all(class_="text-left")[0].get_text()
+            CLUB = stat.find_all(class_="text-left")[1].get_text()
+            POSITION =  stat.find(class_="text-left text-dark").get_text()
+            HEIGHT = stat.find_all(class_="text-center text-dark")[0].get_text()
+            FOOT = stat.find_all(class_="text-center text-dark")[1].get_text()
+            AGE = stat.find_all(class_="text-center text-dark")[2].get_text()
+            RATING = stat.find_all(class_="text-center text-dark")[3].get_text()
+
+            # print(NAME, CLUB, POSITION, HEIGHT, FOOT, AGE , RATING)
+            # print(HEIGHT)
+            
+            # Update our list of player stats
+            player_stats.append((NAME, CLUB, POSITION, HEIGHT, FOOT, AGE , RATING))
+    print("--- Successfully Done with Scraping ---")
+
+# Writing our scraped stats to a file
+def write_stats_to_file():
+    # Get the top 100 from 120
+    top_100_players = player_stats[0:101]
+    
+    #Include Headers in our CSV
+    HEADERS = ["NAME", "CLUB", "POSITION", "HEIGHT", "FOOT", "AGE" , "RATING"]
+    
+    # CREATE A DATAFRAME for top 100
+    stats_dataframe = pd.DataFrame(top_100_players, columns=HEADERS)
+    stats_dataframe.to_csv(output_file)
+    
+    print(f"Successfully created {output_file} file ")
+        
+# # Running our scraper
+build_urls()
+get_stats()  
+write_stats_to_file()
